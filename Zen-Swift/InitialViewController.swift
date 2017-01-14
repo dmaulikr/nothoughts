fe64cb4e46af0a2bb3b3278c98bd349bb6a909e7
Ricardo Nazario <nazarioricardo@gmail.com>
2017-01-14 16:25:16 -0400

    Page view working

--- Zen-Swift/InitialViewController.swift
+++ Zen-Swift/InitialViewController.swift
@@ -9,32 +9,90 @@
 import UIKit
 
 
-class InitialViewController: UIViewController  {
+class InitialViewController: UIViewController, UIPageViewControllerDataSource {
     
+    var pageIndex: Int = 0
+    var pageViewController: UIPageViewController?
     let pathArray = ["Right View", "Right Thinking", "Right Mindfulness", "Right Speech", "Right Action", "Right Diligence", "Right Concentration", "Right Livelihood"]
-    
-    @IBOutlet weak var lessonLabel: UILabel!
-    @IBOutlet var swipeGestureRecognizer: UISwipeGestureRecognizer!
-    
-    @IBAction func swipe(_ sender: Any) {
-        
-        if swipeGestureRecognizer.direction == .left {
-            
-            print("Left")
-            
-        }
-        
-    }
     
     @IBAction func beginPressed(_ sender: Any) {
         
         self.performSegue(withIdentifier: "MeditationSegue", sender: self)
     }
     
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    
+//        let pageContent: PageContentViewController = viewController as! PageContentViewController
+        
+        let pageContentVC = viewController as! PageContentViewController
+        let currentIndex = pageContentVC.index
+        let previousIndex = abs((currentIndex! - 1) % pathArray.count)
+        
+        if pageIndex == NSNotFound {
+            return nil
+        }
+        
+        return viewControllerAtIndex(index: previousIndex)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        let pageContentVC = viewController as! PageContentViewController
+        let currentIndex = pageContentVC.index
+        let nextIndex = abs((currentIndex! + 1) % pathArray.count)
+
+        if (pageIndex == NSNotFound) {
+            return nil;
+        }
+        
+        return viewControllerAtIndex(index: nextIndex)
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pathArray.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    func viewControllerAtIndex(index: NSInteger) -> PageContentViewController {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // Create a new view controller and pass suitable data.
+        let contentViewController = storyboard.instantiateViewController(withIdentifier: "PageContentViewController") as! PageContentViewController
+        
+        contentViewController.lessonTitle = pathArray[index]
+        contentViewController.index = index
+        
+        return contentViewController
+    }
+    
+    func presentPageController() {
+        
+        pageViewController = self.storyboard?.instantiateViewController(withIdentifier: "PageViewController") as! UIPageViewController!
+        pageViewController?.dataSource = self
+        
+        let startingViewController = self.viewControllerAtIndex(index: 0)
+        let viewControllers: [UIViewController] = [startingViewController]
+        
+        self.pageViewController?.setViewControllers(viewControllers, direction: .reverse, animated: true, completion: nil)
+        
+        // Change size
+        self.pageViewController?.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: 75)
+        self.pageViewController?.view.center = self.view.center
+        
+        self.addChildViewController(pageViewController!)
+        self.view.addSubview((pageViewController?.view)!)
+        self.pageViewController?.didMove(toParentViewController: self)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
+        self.presentPageController()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Zen-Swift/PageContentViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PageContentViewController.swift
+//  Zen-Swift
+//
+//  Created by Ricardo Nazario on 1/14/17.
+//  Copyright © 2017 Ricardo Nazario. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+    
+    @IBOutlet weak var lessonLabel: UILabel!
+    
+    var lessonTitle: String! = nil
+    var index: Int! = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        lessonLabel.text = lessonTitle
+        print(index)
+        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.8980433345, blue: 0.8181681037, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
