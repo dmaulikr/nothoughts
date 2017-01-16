c81d7fdd997ce5b29eef042d69417c8ce3b4dbdf
Ricardo Nazario <nazarioricardo@gmail.com>
2017-01-16 11:45:51 -0400

    Added segmented controller, and reading selected lesson

---
+++ Zen-Swift/SegmentedController.swift
@@ -0,0 +1,21 @@
+//
+//  SegmentedController.swift
+//  Zen-Swift
+//
+//  Created by Ricardo Nazario on 1/16/17.
+//  Copyright © 2017 Ricardo Nazario. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedController: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
